c7380ddc930e8d74deefe62a7d47160f1008a9dd
romariojhonatan <romario_manda@hotmail.com>
2018-08-20 21:20:07 -0300

    Criação da classe Aluno e criação automatica do banco de dados H2

---
+++ PrimeiroProjeto/src/main/java/br/com/senac/dominio/Aluno.java
@@ -0,0 +1,42 @@
+package br.com.senac.dominio;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String nome;
+	
+	public Aluno(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
